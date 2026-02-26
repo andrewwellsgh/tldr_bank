44a68effa9cd44ad1fa9e7333b10a7d9430a1847
lizard <10530797+30405208@users.noreply.github.com>
2026-02-26 14:15:56 +0000

    Displaying graph and stats

--- src/tldr_bank/keyword_manager.py
+++ src/tldr_bank/keyword_manager.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 class KeywordManager:
-    def __init__(self, df):
+    def __init__(self, df, currency='GBP'):
         self.df = df
+        self.currency = currency
 
-    def run(self):
+    def run(self, reverse=False):
         df = self.df.copy()
         df['keyword'] = df['description'].str.lower().str.strip()
-        totals = df.groupby('keyword')['amount'].sum().sort_values(ascending=False)
+        totals = df.groupby('keyword')['amount'].sum().sort_values(ascending=not reverse)
         return totals
